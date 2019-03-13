8926698f6daa31691d3db38bc3bc49434932c29b
BillW01 <ateeq.rehmani@ideofuzion.com>
2019-03-13 18:33:01 +0500

    airdrop fix

--- cron/airdrop.js
+++ cron/airdrop.js
@@ -1,7 +1,7 @@
 let cron = require('node-cron')
 let rp = require('request-promise')
 
-let apiUrlForTransfers = `${process.env.TRON_SCAN_URL}api/transfer`
+let apiUrlForTransfers = `${process.env.TRON_SCAN_URL}api/transfer?limit=50`
 let apiUrlForAddressDetails = `${process.env.TRON_SCAN_URL}api/account?address=`
 
 let db = global.healthportDb
@@ -19,6 +19,7 @@ let options = {
 
 let task = cron.schedule('*/15 * * * *', async () => {
     try {
+        console.log('Airdrop cron started')
         let promisesArray = [], err;
         //DB Queries
         [err, airDropUsersCount] = await utils.to(db.models.air_drop_users.count())
@@ -41,27 +42,22 @@ let task = cron.schedule('*/15 * * * *', async () => {
         if (!rewardObj[0].cron_job_status) {
             return;
         }
+
         //Getting Transactions which are on TRON Network
         options.uri = apiUrlForTransfers
         let response = await rp(options)
+
         // Filtering Transactions whose Transaction amount is greater or equal to 100 TRX
-        let filteredTransactions = response.data.filter(x => x.amount >= rewardObj[0].reward_amount * Math.pow(10, process.env.DECIMALS))
+        let filteredTransactions = response.data.filter(x => x.amount >= rewardObj[0].max_users * Math.pow(10, process.env.DECIMALS))
+
         for (let i = 0; i < filteredTransactions.length; i++) { // Loop For Filtered Transactions
-            //Getting Detail of Address from the TRON Network API
-            options.uri = `${apiUrlForAddressDetails}${filteredTransactions[i].transferToAddress}`
-            //Pushing Request Promise in Promises Array
-            promisesArray.push(rp(options))
-        }// End of FOR
-        //Resolving all Promises which are in promisesArray
-        let promises_response = await Promise.all(promisesArray)
-        let filtered_promises_array = filterBalances(promises_response, process.env.TRON_TOKEN_KEY)
-        console.log(filtered_promises_array)
-        for (let i = 0; i < filtered_promises_array.length; i++) {
-            if (filtered_promises_array[i].address) {
-                await sendEHRTokensToAirDropUsers(filtered_promises_array[i].address, rewardObj[0].reward_amount);
-                console.log('Airdrop sent')
+            let balance = await tronUtils.getTRC10TokenBalance('', filteredTransactions[i].transferToAddress)
+            if (balance == 0) {
+                await sendEHRTokensToAirDropUsers(filteredTransactions[i].transferToAddress, rewardObj[0].reward_amount)
+            } else {
+                console.log('Found EHR in ', filteredTransactions[i].transferToAddress)
             }
-        }
+        }// End of FOR
     }
     catch (exp) {
         console.log(exp)
