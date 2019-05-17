f3f9384c5fe2f0aa5bd1ebfe4efdd63b29dd58d6
BillW01 <ateeq.rehmani@ideofuzion.com>
2019-05-17 11:55:40 +0500

    HP-327

--- http/controllers/feature/featureController.js
+++ http/controllers/feature/featureController.js
@@ -1,17 +1,65 @@
+const utils = require('../../../etc/utils')
 const response = require('../../../etc/response')
+const resCode = require('../../../enum/responseCodesEnum')
+const resMessage = require('../../../enum/responseMessagesEnum')
 
-async function addFeature(req, res){
-	try{
+const db = global.healthportDb
+
+async function addFeature(req, res) {
+	try {
 		const features = JSON.parse(req.body.features)
-		
+
 		console.log(features)
 
-	}catch (error) {
-        console.log(error)
-        return response.errReturned(res, error)
-    }
+	} catch (error) {
+		console.log(error)
+		return response.errReturned(res, error)
+	}
+}
+
+async function getAllFeatures(req, res) {
+	try {
+		const { id } = req.auth
+		let err = {}, admin = {}, features = {};
+
+		//Verifying user authenticity
+		[err, admin] = await utils.to(db.models.admins.findOne({ where: { id } }))
+		if (err) return response.errReturned(res, err)
+		if (!admin || admin.length == 0)
+			return response.sendResponse(res, resCode.NOT_FOUND, resMessage.USER_NOT_FOUND);
+
+		//Quering db to get list of featuress
+		[err, features] = await utils.to(db.query(`
+			Select id, name, parent_id as parentId, is_feature as isFeature, 
+			sequence, createdAt 
+			from features`,
+			{
+				type: db.QueryTypes.SELECT,
+			}))
+		if (err) return response.errReturned(res, err)
+		if (!features || features.length == 0)
+			return response.sendResponse(res, resCode.NOT_FOUND, resMessage.NO_RECORD_FOUND)
+
+		//Making custom json
+		const featuresArray = []
+		for (let i = 0; i < features.length; i++) {
+			if (features[i].parentId == 0) {
+				featuresArray.push(features[i])
+				featuresArray[featuresArray.length - 1].children = (features.filter(x => x.parentId ==
+					featuresArray[featuresArray.length - 1].id))
+			}
+		}
+
+		//Returing successful response with data
+		return response.sendResponse(res, resCode.SUCCESS, resMessage.SUCCESS, featuresArray)
+
+	} catch (error) {
+		console.log(error)
+		return response.errReturned(res, error)
+	}
 }
 
 module.exports = {
-    addFeature,
+	addFeature,
+	getAllFeatures
 }
--- http/controllers/role/roleController.js
+++ http/controllers/role/roleController.js
@@ -7,12 +7,19 @@ const db = global.healthportDb
 
 async function getAllRoles(req, res) {
 	try {
+		const { id } = req.auth
 		const obj = {
 			'searchValue': req.body.searchValue,
 			'filter': req.body.status,
 		}
-		let err = {}, dbData = {}
-		const returnableData = {}
+		let err = {}, dbData = {}, admin = {}
+		const returnableData = {};
+
+		//Verifying user authenticity
+		[err, admin] = await utils.to(db.models.admins.findOne({ where: { id } }))
+		if (err) return response.errReturned(res, err)
+		if (!admin || admin.length == 0)
+			return response.sendResponse(res, resCode.NOT_FOUND, resMessage.USER_NOT_FOUND)
 
 		//Paging
 		let pageSize = parseInt(req.body.pageSize)
@@ -60,6 +67,71 @@ async function getAllRoles(req, res) {
 	}
 }
 
+async function getRoleByID(req, res) {
+	try {
+		const { id } = req.auth
+		const { roleId } = req.params
+
+		let err = {}, admin = {}, role = {};
+
+		//Verifying user authenticity
+		[err, admin] = await utils.to(db.models.admins.findOne({ where: { id } }))
+		if (err) return response.errReturned(res, err)
+		if (!admin || admin.length == 0)
+			return response.sendResponse(res, resCode.NOT_FOUND, resMessage.USER_NOT_FOUND);
+
+		[err, role] = await utils.to(db.query(`
+        Select r.name roleName, f.name as featureName, r.id as roleId, r.name as roleName, f.id as featureId,
+			r.description as roleDescription, f.parent_id as parentId, f.is_feature as isFeature, 
+			f.sequence as sequence 
+            From permissions p 
+            Inner join features f ON p.feature_id = f.id
+            Inner join roles r ON r.id = p.role_id
+            Where p.role_id = :roleId and r.status = :status`,
+			{
+				replacements: { roleId: parseInt(roleId), status: true },
+				type: db.QueryTypes.SELECT,
+			}))
+		if (err) return response.errReturned(res, err)
+		if (!role || role.length == 0)
+			return response.sendResponse(res, resCode.NOT_FOUND, resMessage.NO_RECORD_FOUND)
+
+		const features = []
+		for (let i = 0; i < role.length; i++) {
+			if (role[i].parentId == 0) {
+				features.push(role[i])
+				features[features.length - 1].children = (role.filter(x => x.parentId ==
+					features[features.length - 1].featureId))
+			}
+		}
+
+		//Returing successful response with data
+		const data = {
+			id: role[0].roleId,
+			name: role[0].roleName,
+			description: role[0].roleDescription,
+			listFeatures: features
+		}
+		return response.sendResponse(res, resCode.SUCCESS, resMessage.SUCCESS, data)
+
+	} catch (error) {
+		console.log(error)
+		return response.errReturned(res, error)
+	}
+}
+
+async function addNewRole(req, res) {
+	try {
+		//const { name, description, status, features } = req.body
+		//let err = {}, role = {};
+
+	} catch (error) {
+		console.log(error)
+		return response.errReturned(res, error)
+	}
+}
 module.exports = {
-	getAllRoles
+	getAllRoles,
+	getRoleByID,
+	addNewRole
 }
