31c3beeb4bd73f0b3ae05480507e8d68c6445a60
BillW01 <ateeq.rehmani@ideofuzion.com>
2019-06-11 15:06:44 +0500

    app.js modified

--- app.js
+++ app.js
@@ -38,6 +38,4 @@ global.healthportDb.authenticate()
   .then(() => console.log('Db Connected'))
   .catch(err => console.log(err))
 
-console.log('Env var pvt key: ', process.env.MAIN_ACCOUNT_PRIVATE_KEY)
-
 module.exports = app
