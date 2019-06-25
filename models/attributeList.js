5042183393ba1be83302403cbb377a4d0f2a556a
BillW01 <ateeq.rehmani@ideofuzion.com>
2019-06-25 11:15:03 +0500

    HP-500

--- models/attributeList.js
+++ models/attributeList.js
@@ -10,10 +10,5 @@ module.exports = (sequelize, Sequelize) => {
             allowNull: false
         }
     })
-
-    attributeLists.associate = (models) => {
-		attributeLists.hasMany(models.attribute_list_values, { foreignKey: 'list_id' })
-    }
-    
     return attributeLists
 }
