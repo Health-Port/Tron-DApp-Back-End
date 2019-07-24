bca5b1134d26402c37a288468bc6df2e591189e5
Ateeq Rehmani <ateeq.rehmani@IdeoFuzion.Ltd>
2019-07-24 16:52:47 +0500

    Logout Implementation on User Blockage

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -44,8 +44,8 @@ async function authenticateRole(req, res, next) {
             const filterArray = permisions.filter(x => x.route == req.route.path)
             if (!(filterArray.length > 0))
                 return response.sendResponse(
-                    res, 
-                    resCode.BAD_REQUEST, 
+                    res,
+                    resCode.BAD_REQUEST,
                     'Access Denied.')
         }
         next()
@@ -55,7 +55,40 @@ async function authenticateRole(req, res, next) {
     }
 }
 
+async function blockage(req, res, next) {
+    let err = {}, admin = {}
+    const token = req.headers.authorization
+    if (!token) return response.sendResponse(res, resCode.UNAUTHORIZED, resMessage.ACCESS_DENIED)
+
+    try {
+        const verifiedTotken = jwt.verify(token, process.env.SECRET);
+
+        [err, admin] = await utils.to(db.query(`
+        Select a.status as adminStatus, r.status as roleStatus from admins a 
+            inner join roles r ON a.role_id  = r.id
+            Where a.id = :id`,
+            {
+                replacements: { id: verifiedTotken.id },
+                type: db.QueryTypes.SELECT,
+            }))
+        if (err) return response.errReturned(res, err)
+        if (!admin || admin == null || admin.length == 0)
+            return response.sendResponse(res, resCode.UNAUTHORIZED, resMessage.USER_NOT_FOUND)
+
+        if (!admin[0].adminStatus)
+            return response.sendResponse(res, resCode.UNAUTHORIZED, resMessage.USER_IS_BLOCKEd)
+
+        if (!admin[0].roleStatus)
+            return response.sendResponse(res, resCode.UNAUTHORIZED, resMessage.ROLE_IS_BLOCKED)
+
+        next()
+    } catch (error) {
+        console.log(error)
+        return response.sendResponse(res, resCode.UNAUTHORIZED, resMessage.INVALID_TOKEN)
+    }
+}
 module.exports = {
     authenticateToken,
-    authenticateRole
+    authenticateRole,
+    blockage
 }
