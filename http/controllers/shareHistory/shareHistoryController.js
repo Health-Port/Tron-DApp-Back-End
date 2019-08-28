b4adfdb5a0002f9ef67fe79e41b8223001bf4fc4
Ateeq Rehmani <ateeq.rehmani@IdeoFuzion.Ltd>
2019-08-28 13:04:23 +0500

    Medical Record Share History API

--- http/controllers/shareHistory/shareHistoryController.js
+++ http/controllers/shareHistory/shareHistoryController.js
@@ -146,7 +146,86 @@ async function updateRights(req, res) {
 	}
 }
 
+async function getMedicalRecordHisotry(req, res) {
+	try {
+		const { user_id } = req.auth
+		const { medicalRecordId } = req.body
+		let { pageNumber, pageSize } = req.body
+
+		let err = {}, user = {}, shareHistory = {}
+
+		//Paging
+		pageSize = parseInt(pageSize)
+		pageNumber = parseInt(pageNumber)
+		if (!pageNumber) pageNumber = 0
+		if (!pageSize) pageSize = 10
+		const start = parseInt(pageNumber * pageSize)
+		const end = parseInt(start + pageSize)
+
+		if (!medicalRecordId)
+			return response.sendResponse(res, resCode.BAD_REQUEST, resMessage.REQUIRED_FIELDS_EMPTY);
+
+		//Verifying user authenticity  
+		[err, user] = await utils.to(db.models.users.findOne({ where: { id: user_id } }))
+		if (err) return response.errReturned(res, err)
+		if (user == null)
+			return response.sendResponse(res, resCode.NOT_FOUND, resMessage.USER_NOT_FOUND);
+
+		//Querying db for list records
+		[err, shareHistory] = await utils.to(db.query(`
+		Select sh.id as shareHistoryId, u.name, u.email, st.name as permission, sh.createdAt 
+			From share_histories sh
+			Inner join users u ON sh.share_with_user_id = u.id
+			Inner join share_rights sr ON sr.share_history_id = sh.id
+			Inner join share_types st ON st.id = sr.share_type_id
+			Where sh.medical_record_id = :mId and sh.share_from_user_id = :uId
+			ORDER BY sh.createdAt DESC 
+			`,
+			//LIMIT ${start}, ${pageSize}
+			{
+				replacements: { mId: medicalRecordId, uId: user_id },
+				type: db.QueryTypes.SELECT,
+			}))
+		if (err) return response.errReturned(res, err)
+		if (!shareHistory || shareHistory.length == 0)
+			return response.sendResponse(res, resCode.NOT_FOUND, resMessage.NO_RECORD_FOUND)
+
+		let data = []
+		const returnableData = {}
+		for (let i = 0; i < shareHistory.length; i++) {
+			const filterArry = shareHistory.filter(x => x.shareHistoryId == shareHistory[i].shareHistoryId)
+			if (filterArry.length > 1) {
+				data[i] = {
+					id: shareHistory[i].shareHistoryId,
+					sharedWith: `${shareHistory[i].name}, ${shareHistory[i].email}`,
+					permission: filterArry.map(x => x.permission),
+					createdAt: shareHistory[i].createdAt
+				}
+				i++
+			} else {
+				data[i] = {
+					id: shareHistory[i].shareHistoryId,
+					sharedWith: `${shareHistory[i].name}, ${shareHistory[i].email}`,
+					permission: [shareHistory[i].permission],
+					createdAt: shareHistory[i].createdAt
+				}
+			}
+		}
+		data = data.filter((el) => { return el != null })
+		returnableData['count'] = data.length
+		const slicedData = data.slice(start, end)
+		returnableData['rows'] = slicedData
+
+		//Returing successful response
+		return response.sendResponse(res, resCode.SUCCESS, resMessage.SUCCESS, returnableData)
+
+	} catch (error) {
+		console.log(error)
+		return response.errReturned(res, error)
+	}
+}
 module.exports = {
 	addShareHistory,
-	updateRights
+	updateRights,
+	getMedicalRecordHisotry
 }
